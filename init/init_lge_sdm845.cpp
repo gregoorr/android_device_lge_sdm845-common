574fb1a987a2e559adf1034b47cf7d5b6c41d477
GPJ <grzegorz.jaworski1@gmail.com>
2023-02-12 13:31:47 +0100

    sdm845:-common: Update Cheetah fingerprint to February 2023 release

--- init/init_lge_sdm845.cpp
+++ init/init_lge_sdm845.cpp
@@ -144,5 +144,5 @@ void vendor_load_properties() {
     init_target_properties();
     LOG(INFO) << "Loading Cheetah Fingerprint";
 	property_override_multifp("ro.build.fingerprint", "ro.system.build.fingerprint", "ro.bootimage.build.fingerprint",
-	    "ro.vendor.build.fingerprint", "google/cheetah/cheetah:13/TQ1A.230105.002.A1/9409646:user/release-keys");
+	    "ro.vendor.build.fingerprint", "google/cheetah/cheetah:13/TQ1A.230205.002/9471150:user/release-keys");
 }
