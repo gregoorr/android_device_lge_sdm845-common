fc4af1f0f41646726968cf2c64e472ee4e32b345
GPJ <grzegorz.jaworski1@gmail.com>
2023-02-16 13:52:52 +0100

    Build Aperture instead of Snap

--- sdm845.mk
+++ sdm845.mk
@@ -186,7 +186,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
-    Snap \
+    Aperture \
     vendor.qti.hardware.camera.device@1.0.vendor
 
 # Context Hub
