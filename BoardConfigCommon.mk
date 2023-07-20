7a7a4464b28fe1a3760650bc036cde2207c79267
GPJ <grzegorz.jaworski1@gmail.com>
2023-07-20 23:36:51 +0200

    sdm845-common: Things
    Change architecture
    Exclude musicfx
    Set target ota

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -21,17 +21,22 @@ COMMON_PATH := device/lge/sdm845-common
 
 # Architecture
 TARGET_ARCH := arm64
-TARGET_ARCH_VARIANT := armv8-2a
+TARGET_ARCH_VARIANT := armv8-2a-dotprod
 TARGET_CPU_ABI := arm64-v8a
-TARGET_CPU_VARIANT := kryo385
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := cortex-a76
 
-ifeq (,$(filter %_64,$(TARGET_PRODUCT)))
 TARGET_2ND_ARCH := arm
 TARGET_2ND_ARCH_VARIANT := armv8-2a
 TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
-TARGET_2ND_CPU_VARIANT := kryo385
-endif
+TARGET_2ND_CPU_VARIANT := cortex-a76
+
+# Assert
+TARGET_OTA_ASSERT_DEVICE := judypn
+
+# Audio
+TARGET_USE_MUSICFX := false
 
 # Bootloader
 TARGET_BOOTLOADER_BOARD_NAME := sdm845
